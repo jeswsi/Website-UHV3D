b24b11a1fc705062a3d0bf274852826606e678ce
jeswsi <james@woodruffscientific.com>
2016-12-02 12:03:44 -0800

    Made Examples Page
    Added some images of RFA and Beam Dump
    Made an Examples page

---
+++ examples.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>UHV3D - Design for Ultra High Vacuum Environments</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.css">
+    <link rel="stylesheet" href="vendor/device-mockups/device-mockups.min.css">
+
+    <!-- Theme CSS -->
+    <link href="css/new-age.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top">
+
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="http://uhv3d.com">UHV 3D</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="http://uhv3d.com#features">About</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+
+
+
+    <section id="product" class="product thin"></section>
+
+    <section id="features" class="features">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="section-heading">
+                        <h2>Example Products from UHV3D</h2>
+                        <p class="text-muted">We can take your design from the concept stage all the way to a finished product. Our services include: Engineering Design, Optimization for Additive Manufacture, Material Suitablity Analysis, Engineering Analysis, Manufacture, and more.</p>
+                        <br>
+                        <a href="#contact" class="btn btn2-outline btn-xl page-scroll">Contact Our Engineers!</a>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+
+                <div class="col-md-12">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <h2>3D Printed Tungsten Retarding Field Analyzer</h2>
+                            <div class="col-md-4">
+                                <div class="feature-item">
+                                    <!-- <i class="icon-screen-smartphone text-primary"></i> -->
+                                    <img src="img/RFA_CAD_small.PNG">
+                                    <p class="text-muted">RFA Probe Head Design</p>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="feature-item">
+                                    <!-- <i class="icon-screen-smartphone text-primary"></i> -->
+                                    <img src="img/RFA_COMSOL_small.PNG">
+                                    <p class="text-muted">Optimizing Design for Heat Load</p>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="feature-item">
+                                    <!-- <i class="icon-screen-smartphone text-primary"></i> -->
+                                    <img src="img/rfa_probe_small.jpg">
+                                    <p class="text-muted">Probe Head Mounted on NSTX</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <h2>Beam Dump</h2>
+                            <div class="col-md-4">
+                                <div class="feature-item">
+                                    <!-- <i class="icon-screen-smartphone text-primary"></i> -->
+                                    <img src="img/Beam_Dump_Section_Small.png">
+                                    <p class="text-muted">Beam Dump Design</p>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="feature-item">
+                                    <img src="img/Zemax_analysis_small.PNG">
+                                    <p class="text-muted">Light Reduction Analysis in Zemax</p>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="feature-item">
+                                    <img src="img/beam_dump_printed.jpg">
+                                    <p class="text-muted">3D Printed Beam Dump</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>
+
+    <!-- <section class="cta">
+        <div class="cta-content">
+            <div class="container">
+                <h2>Stop waiting.<br>Start building.</h2>
+                <a href="#contact" class="btn btn-outline btn-xl page-scroll">Let's Get Started!</a>
+            </div>
+        </div>
+        <div class="overlay"></div>
+    </section> -->
+
+    <section id="contact" class="contact bg-primary">
+        <div class="container">
+            <h2>Email UHV3D</h2>
+            <a href="mailto:info@uhv3d.com" class="btn btn-outline btn-xl">info@uhv3d.com</a>
+            <!-- <h2>We <i class="fa fa-heart"></i> new friends!</h2>
+            <ul class="list-inline list-social">
+                <li class="social-twitter">
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                </li>
+                <li class="social-facebook">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                </li>
+                <li class="social-google-plus">
+                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                </li>
+            </ul> -->
+        </div>
+    </section>
+
+    <footer>
+        <div class="container">
+            <p>&copy; 2016 UHV3D. All Rights Reserved.</p>
+            <ul class="list-inline">
+<!--                 <li>
+                    <a href="#">Privacy</a>
+                </li>
+                <li>
+                    <a href="#">Terms</a>
+                </li> 
+                <li>
+                    <a href="#features">FAQ</a>
+                </li>-->
+            </ul>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/new-age.min.js"></script>
+
+</body>
+
+</html>
