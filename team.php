96fd351ccf6b9da3adf68e5171ea0dd056c8b5e1
jeswsi <james@woodruffscientific.com>
2016-12-15 10:57:43 -0800

    add a team page

---
+++ team.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Team | UHV3D - Team</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.css">
+    <link rel="stylesheet" href="vendor/device-mockups/device-mockups.min.css">
+
+    <!-- Theme CSS -->
+    <link href="css/new-age.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top">
+
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="http://uhv3d.com">UHV 3D</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="http://uhv3d.com#features">About</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#features">Examples</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#employees">Team</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+
+
+
+    <section id="product" class="product thin"></section>
+
+    <section id="features" class="features">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="section-heading">
+
+                        <h2>UHV3D's Team</h2>
+                        <p class="text-muted">We can take your design from the concept stage all the way to a finished product. Our services include: Engineering Design, Optimization for Additive Manufacture, Material Suitablity Analysis, Engineering Analysis, Manufacture, and more.</p>
+
+                        <br>
+                        <a href="#contact" class="btn btn2-outline btn-xl page-scroll">Contact Our Engineers!</a>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+
+                <div class="col-md-12">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <h2 class="text-center">UHV3D's Engineers</h2>
+                            <div class="col-md-4">
+                                <div class="feature-item">
+                                    <!-- <i class="icon-screen-smartphone text-primary"></i> -->
+                                    <img src="img/Headshots/simon.jpg" alt="Image of Simon Woodruff" style="width:128px;" align="left" Hspace="10">
+                                    <h3><strong><a href="http://www.researchgate.net/profile/Simon_Woodruff/">Dr. Simon Woodruff</a>
+                                    </strong><span class="text-muted">Physicist</span></h3>
+                                    <p>After a decade of research into innovative fusion concepts at the University of Manchester (PhD), Lawrence Livermore National Lab (Postdoc), UC Berkeley Department of Nuclear Engineering (Visiting Scholar), and the University of Washington (Research Scientist), Dr. Woodruff founded WSI to help accelerate the development of economic fusion energy.   Woodruff has performed work under contract to DOE, universities, national labs and to the private sector, building research devices, performing modeling and simulations primarily directed at simply-connected (hence more compact and simpler-to-engineer) fusion systems.  Dr. Woodruff is a member of the American Physical Society and the Institute of Physics.</p>
+                                    <p>For publications, please visit <a href="http://www.researchgate.net/profile/Simon_Woodruff/">ResearchGate</a></p>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="feature-item">
+                                    <!-- <i class="icon-screen-smartphone text-primary"></i> -->
+                                    <img src="img/Headshots/paul2.jpg" alt="Image of Paul Sieck" style="width:128px;" align="left" Hspace="10">
+
+                                    <h3><strong><a href="https://www.researchgate.net/profile/Paul_Sieck">Dr. Paul Sieck</a></strong><span class="text-muted"> Engineer, Physicist</span></h3>
+
+                                    <p>Dr. Sieck has 16 years experience in laboratory plasma physics and
+                                    nuclear fusion research.  He has worked in a diverse range of
+                                    experimental programs, from his doctoral work on the HIT-SI spheromak
+                                    experiment at the University of Washington, to the DIII-D tokamak at
+                                    General Atomics, and most recently the Polywell program at Energy Matter
+                                    Conversion Corporation.  He has broad experience in magnetic
+                                    measurements, signal conditioning, data acquisition, laboratory
+                                    automation, and feedback control of plasmas.  His research interests
+                                    include high-beta MHD, accelerated plasma flows, driven magnetic
+                                    reconnection, and plasma dynamos.</p>
+                                    <p>For publications, please visit <a href="https://www.researchgate.net/profile/Paul_Sieck">ResearchGate</a></p>
+
+                                    <br></br>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="feature-item">
+                                    <!-- <i class="icon-screen-smartphone text-primary"></i> -->
+                                    <img src="img/Headshots/morgan.jpg" alt="Image of Morgan Quinley" style="width:128px;" align="left" Hspace="10">
+
+                                    <h3><strong><a href="https://www.researchgate.net/profile/Morgan_Quinley/">Morgan Quinley</a></strong><span class="text-muted"> Diagnostics Engineer</span></h3>
+
+                                    <p>Morgan earned a bachelor's degree in aerospace engineering from Saint Louis University, and during that time held internships at NASA and Andrews Space. He then worked for a few years as an R&D engineer in the field of nuclear medicine before starting graduate school in plasma science at the University of Washington. While at UW, Morgan helped to build an astrophysical jet experiment, ultimately writing his master's thesis on the design and implementation of a retarding field analyzer.</p>
+                                    <p>For publications, please visit <a href="https://www.researchgate.net/profile/Morgan_Quinley/">ResearchGate</a></p>
+
+                                    <br></br>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>
+
+    <!-- <section class="cta">
+        <div class="cta-content">
+            <div class="container">
+                <h2>Stop waiting.<br>Start building.</h2>
+                <a href="#contact" class="btn btn-outline btn-xl page-scroll">Let's Get Started!</a>
+            </div>
+        </div>
+        <div class="overlay"></div>
+    </section> -->
+
+    <section id="contact" class="contact bg-primary">
+        <div class="container">
+            <h2>Email UHV3D</h2>
+            <a href="mailto:info@uhv3d.com" class="btn btn-outline btn-xl">info@uhv3d.com</a>
+            <!-- <h2>We <i class="fa fa-heart"></i> new friends!</h2>
+            <ul class="list-inline list-social">
+                <li class="social-twitter">
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                </li>
+                <li class="social-facebook">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                </li>
+                <li class="social-google-plus">
+                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                </li>
+            </ul> -->
+        </div>
+    </section>
+
+    <footer>
+        <div class="container">
+            <p>&copy; 2016 UHV3D. All Rights Reserved.</p>
+            <ul class="list-inline">
+<!--                 <li>
+                    <a href="#">Privacy</a>
+                </li>
+                <li>
+                    <a href="#">Terms</a>
+                </li> 
+                <li>
+                    <a href="#features">FAQ</a>
+                </li>-->
+            </ul>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/new-age.min.js"></script>
+
+    <!-- Google Analytics -->
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-88444550-1', 'auto');
+  ga('send', 'pageview');
+    </script>
+
+</body>
+
+</html>
